2cc30fff6acd17b664e395da434e9ed6ff5a9fad
valyy151 <marin.valenta1@gmail.com>
2023-04-07 12:05:41 +0200

    CreateApartment component creataed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import ironhackersImg from './assets/ironhackers.avif'
 import './App.css'
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import HomePage from './components/HomePage'
@@ -10,19 +10,19 @@ import CreateApartment from './components/CreateApartment'
 import axios from 'axios'
 
 function App() {
-const baseURL = "https://ironbnb-m3.herokuapp.com";
-const [apartmentsArr, setApartmentsArr] = useState([]);
+	const baseURL = 'https://ironbnb-m3.herokuapp.com'
+	const [apartmentsArr, setApartmentsArr] = useState([])
 
-useEffect(() => {
-  axios
-    .get(baseURL + "/countries")
-    .then((response) => {
-      setApartmentsArr(response.data);
-    })
-    .catch((err) => {
-      console.log("error getting apartments from API", err);
-    });
-}, []);
+	useEffect(() => {
+		axios
+			.get(baseURL + '/countries')
+			.then((response) => {
+				setApartmentsArr(response.data)
+			})
+			.catch((err) => {
+				console.log('error getting apartments from API', err)
+			})
+	}, [])
 
 	return (
 		<Router>
@@ -30,24 +30,20 @@ useEffect(() => {
 				<NavBar />
 				<Routes>
 					<Route
-						exact
 						path='/'
-						component={HomePage}
+						element={<HomePage />}
 					/>
 					<Route
-						exact
 						path='/apartments'
-						component={ApartmentsList}
+						element={<ApartmentsList />}
 					/>
 					<Route
-						exact
 						path='/apartments/:id'
-						component={ApartmentsDetails}
+						element={<ApartmentsDetails />}
 					/>
 					<Route
-						exact
 						path='/apartments/create'
-						component={CreateApartment}
+						element={<CreateApartment />}
 					/>
 				</Routes>
 			</div>
