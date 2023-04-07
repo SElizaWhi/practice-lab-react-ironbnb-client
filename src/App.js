e847395a3afe565c8f676cae1600b1dccb8510f7
valyy151 <marin.valenta1@gmail.com>
2023-04-07 11:43:19 +0200

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import ironhackersImg from "./assets/ironhackers.avif"
-import './App.css';
+import ironhackersImg from './assets/ironhackers.avif'
+import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
+import HomePage from './components/HomePage'
+import ApartmentsList from './components/ApartmentsList'
+import ApartmentDetails from './components/ApartmentDetails'
+import CreateApartment from './components/CreateApartment'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Welcome</h1>
-      <img src={ironhackersImg} alt="ironhackers" />
-    </div>
-  );
+	return (
+		<Router>
+			<div className='App'>
+				<NavBar />
+				<Routes>
+					<Route
+						exact
+						path='/'
+						component={HomePage}
+					/>
+					<Route
+						exact
+						path='/apartments'
+						component={ApartmentsList}
+					/>
+					<Route
+						exact
+						path='/apartments/:id'
+						component={ApartmentDetails}
+					/>
+					<Route
+						exact
+						path='/apartments/create'
+						component={CreateApartment}
+					/>
+				</Routes>
+			</div>
+		</Router>
+	)
 }
 
-export default App;
+export default App
